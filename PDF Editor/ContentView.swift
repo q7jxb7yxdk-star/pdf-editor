8121795f184ada06e851ec183ca84582699311c2
q7jxb7yxdk-star <q7jxb7yxdk@privaterelay.appleid.com>
2026-08-12 19:18:53 +0800

    Initial Commit

---
+++ PDF Editor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PDF Editor
+//
+//  Created by Sunny Yu on 12/8/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PDF Editor/PDF_EditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDF_EditorApp.swift
+//  PDF Editor
+//
+//  Created by Sunny Yu on 12/8/2026.
+//
+
+import SwiftUI
+
+@main
+struct PDF_EditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
